825bd3d8804547a954dcd235063c5e8d89d5d868
mmport80 <john.orford@gmail.com>
2016-04-20 08:43:56 +0200

    npm

---
+++ app/components/Main.js
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var Main = React.createClass({
+  render: function () {
+    return (
+      <div className='main-container'>
+        <div> Hello from Main! </div>
+        {this.props.children}
+      </div>
+    )
+  }
+});
+
+module.exports = Main;
